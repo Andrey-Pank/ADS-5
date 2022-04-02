28523cc28e93be30c2fd57c07e18aef094bed485
Andrey-Pank <99199092+Andrey-Pank@users.noreply.github.com>
2022-04-02 11:46:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,8 +4,48 @@
 #include "tstack.h"
 
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  TStack <char, 100> stack;
+    std::string out;
+    int i = 0;
+    for (; i < inf.length(); i++) {
+        if (5 == priory(inf[i])) {
+            out += inf[i];
+            while (5 == priory(inf[i+1]) && (i+1) < inf.length()) {
+                out += inf[i+1];
+                i += 1;
+            }
+            out += ' ';
+        } else {
+            if (0 == priory(inf[i])) {
+                stack.push(inf[i]);
+            } else if (priory(inf[i]) > priory(stack.get())) {
+                stack.push(inf[i]);
+            } else if (stack.isEmpty()) {
+                stack.push(inf[i]);
+            } else if (1 == priory(inf[i])) {
+                while (0 != priory(stack.get)) {
+                    out += stack.get();
+                    out += ' ';
+                    stack.pop();
+                }
+                stack.pop();
+            } else if (priory(inf[i]) <= priory(stack.get())) {
+                while (!stack.isEmpty() && 1 < priory(stack.get())) {
+                    out += stack.get();
+                    out += ' ';
+                    stack.pop;
+                }
+                stack.push(inf[i]);
+            }
+        }
+    }
+    while (!stack.isEmpty()) {
+        out += stack.get();
+        out += ' ';
+        stack.pop();
+    }
+    out.pop_back();
+    return out;
 }
 
 int eval(std::string pref) {
